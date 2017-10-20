3ecbb9489573eef8cc1c16f41ebd33826d6fa04e
sc-ido <ido@sitecore.net>
2017-10-20 15:41:42 +0300

    Original code from TFS

---
+++ src/Sitecore.Support.189862/FXM/Client/Pipelines/ExperienceEditor/ExternalPage/InjectControlsProcessor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Sitecore.FXM.Utilities;
+
+namespace Sitecore.Support.FXM.Client.Pipelines.ExperienceEditor.ExternalPage
+{
+  using System.Web.UI;
+
+  using HtmlAgilityPack;
+
+  using Sitecore.Abstractions;
+  using Sitecore.Diagnostics;
+  using Sitecore.FXM.Client.Pipelines.ExperienceEditor.ExternalPage;
+
+  /// <summary>Placeholder control injection pipeline processor.</summary>
+  public class InjectControlsProcessor : ExternalPageProcessorBase, IExternalPageExperienceEditorProcessor
+  {
+    public InjectControlsProcessor()
+      : this(new SettingsWrapper())
+    {
+    }
+
+    public InjectControlsProcessor(ISettings settings)
+      : base(settings)
+    {
+    }
+
+    /// <summary>Generates the Placeholder controls for the external site page.</summary>
+    /// <param name="args">The args.</param>
+    public void Process(ExternalPageExperienceEditorArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      Assert.ArgumentNotNull(args.MatcherContextItem, "MatcherContextItem");
+      Assert.ArgumentNotNullOrEmpty(args.ExternalPageContent, "ExternalPageContent");
+
+      HtmlDocument htmlDoc = new HtmlDocument();
+      htmlDoc.LoadHtml(args.ExternalPageContent);
+
+      LiteralControl headControl = new LiteralControl();
+      var headSingleNode = htmlDoc.DocumentNode.SelectSingleNode("//head");
+      if (headSingleNode != null)
+      {
+        headControl.Text = headSingleNode.InnerHtml;
+      }
+
+      args.HeadControls.Add(headControl);
+
+      string bodyHtml = string.Empty;
+      var bodySingleNode = htmlDoc.DocumentNode.SelectSingleNode("//body");
+      if (bodySingleNode != null)
+      {
+        Uri experienceEditorUrl;
+        if (Uri.TryCreate(args.ExperienceEditorUrl, UriKind.RelativeOrAbsolute, out experienceEditorUrl))
+        {
+          this.HostName = FxmUtility.GetUriHost(experienceEditorUrl);
+        }
+
+        var beaconTag = this.GetBeaconScriptTag(bodySingleNode);
+        if (beaconTag != null)
+        {
+          this.RemoveNode(beaconTag);
+        }
+
+        bodyHtml = bodySingleNode.InnerHtml;
+      }
+
+      args.BodyControls.Add(new LiteralControl(bodyHtml));
+    }
+  }
+}
