0efb820850799eb1fc86549c1e2da55682e24f39
DK\mdo <mdo@sitecore.net>
2018-04-12 16:35:24 -0400

    Modified the code to be compatible with SItecore 9.0 update 1

---
+++ src/Sitecore.Support.189862/FXM/Client/Pipelines/ExperienceEditor/ExternalPage/InjectControlsProcessor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Sitecore.FXM.Utilities;
+using System.Web.UI;
+using HtmlAgilityPack;
+using Sitecore.Abstractions;
+using Sitecore.Diagnostics;
+using Sitecore.FXM.Client.Pipelines.ExperienceEditor.ExternalPage;
+
+namespace Sitecore.Support.FXM.Client.Pipelines.ExperienceEditor.ExternalPage
+{
+  public class InjectControlsProcessor : ExternalPageProcessorBase, IExternalPageExperienceEditorProcessor
+  {
+    public InjectControlsProcessor(BaseSettings settings)
+      : base(settings)
+    {
+    }
+
+    public void Process(ExternalPageExperienceEditorArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      Assert.ArgumentNotNull(args.MatcherContextItem, "MatcherContextItem");
+      Assert.ArgumentNotNullOrEmpty(args.ExternalPageContent, "ExternalPageContent");
+
+      var htmlDoc = new HtmlDocument();
+      htmlDoc.LoadHtml(args.ExternalPageContent);
+
+      var headControl = new LiteralControl();
+      var headSingleNode = htmlDoc.DocumentNode.SelectSingleNode("//head");
+      if (headSingleNode != null)
+      {
+        #region Added code
+        RemoveBeaconFromNode(headSingleNode);
+        #endregion
+        headControl.Text = headSingleNode.InnerHtml;
+      }
+
+      args.HeadControls.Add(headControl);
+
+      var bodyHtml = string.Empty;
+      var bodySingleNode = htmlDoc.DocumentNode.SelectSingleNode("//body");
+      if (bodySingleNode != null)
+      {
+        Uri experienceEditorUrl;
+        if (Uri.TryCreate(args.ExperienceEditorUrl, UriKind.RelativeOrAbsolute, out experienceEditorUrl))
+        {
+          HostName = FxmUtility.GetUriHost(experienceEditorUrl);
+        }
+
+        #region Modified code
+        RemoveBeaconFromNode(bodySingleNode);
+        #endregion
+
+        bodyHtml = bodySingleNode.InnerHtml;
+      }
+
+      args.BodyControls.Add(new LiteralControl(bodyHtml));
+    }
+
+    #region Added code
+    protected virtual void RemoveBeaconFromNode(HtmlNode node)
+    {
+      var beaconTag = this.GetBeaconScriptTag(node);
+      if (beaconTag != null)
+      {
+        this.RemoveNode(beaconTag);
+      }
+    }
+    #endregion
+  }
+}
---
+++ src/Sitecore.Support.189862/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.189862")]
+[assembly: AssemblyProduct("Sitecore.Support.189862")]
+[assembly: ComVisible(false)]
